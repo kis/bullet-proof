dc940ee540e46b0e7e085a321f00818adb44cb64
Kirill S <kirills@Kirills-MacBook-Pro.local>
2022-03-23 22:41:38 +0200

    update

--- target/debug/deps/rust_workshop-cb4fb3428255501d.d
+++
@@ -1,8 +0,0 @@
-/Users/kirills/proj/rust-workshop/target/debug/deps/rust_workshop-cb4fb3428255501d: src/main.rs src/utils.rs src/utils/house_light.rs src/utils/traffic_light.rs
-
-/Users/kirills/proj/rust-workshop/target/debug/deps/rust_workshop-cb4fb3428255501d.d: src/main.rs src/utils.rs src/utils/house_light.rs src/utils/traffic_light.rs
-
-src/main.rs:
-src/utils.rs:
-src/utils/house_light.rs:
-src/utils/traffic_light.rs:
--- target/debug/librust_workshop.d
+++
@@ -1,1 +0,0 @@
-/Users/kirills/proj/rust-workshop/target/debug/librust_workshop.rlib: /Users/kirills/proj/rust-workshop/src/lib.rs
